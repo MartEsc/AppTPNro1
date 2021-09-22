616c933a537d82e3df082a52a426640f0f44674c
Martin Escowich <escowichmartin@gmail.com>
2021-09-22 18:57:58 -0300

    Agrego cuestion de ocultar menúes no necesarios.

--- app/src/main/java/com/EscowichFernandezGayoso/apptpnro1/MainActivity.java
+++ app/src/main/java/com/EscowichFernandezGayoso/apptpnro1/MainActivity.java
@@ -3,23 +3,27 @@ package com.EscowichFernandezGayoso.apptpnro1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView labelDescuento;
     private EditText Titulo, Precio, Direccion_retiro, Email, Descripcion;
     private Spinner Categoria;
     private CheckBox Retiro_en_persona , Terminos_Condiciones;
     private Switch Descuento;
     private SeekBar Cantidad_descuento;
     private RelativeLayout layoutDescuento;
+    private LinearLayout layoutRetiro;
     private Button buttonPublicar;
     private String emailPattern= "[a-zA-Z0-9._-]+@[a-z]{3,}+\\.+[a-z]+";
     private String validador= "[a-zA-Z0-9,. ]+";
@@ -33,18 +37,41 @@ public class MainActivity extends AppCompatActivity {
         Categoria=(Spinner)findViewById(R.id.spinnerCategoria);
         Retiro_en_persona=(CheckBox)findViewById(R.id.checkboxRetiro);
         Terminos_Condiciones =(CheckBox)findViewById(R.id.checkboxTerminos);
+        layoutRetiro=(LinearLayout)findViewById(R.id.layoutRetiro);
         Direccion_retiro=(EditText)findViewById(R.id.editTextDireccion);
         Descuento=(Switch)findViewById(R.id.switchDescuento);
         Cantidad_descuento=(SeekBar)findViewById(R.id.seekbarDescuento);
+        labelDescuento=(TextView)findViewById(R.id.textLabelPorcentajeDescuento);
         Email=(EditText)findViewById(R.id.editTextCorreo);
         Descripcion=(EditText) findViewById(R.id.editTextDescripcion);
         layoutDescuento=(RelativeLayout) findViewById(R.id.layoutDescuento);
         buttonPublicar = (Button) findViewById(R.id.buttonPublicar);
+
         Descuento.setChecked(false);
+        layoutDescuento.setVisibility(View.GONE);
         Terminos_Condiciones.setChecked(false);
         buttonPublicar.setEnabled(false);
+        layoutRetiro.setVisibility(View.GONE);
+        Retiro_en_persona.setChecked(false);
 
 
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        Cantidad_descuento.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                labelDescuento.setText("El descuento es de : "+ seekBar.getProgress() + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void updateSwitchDescuento(View V){
@@ -55,6 +82,16 @@ public class MainActivity extends AppCompatActivity {
                 layoutDescuento.setVisibility(View.GONE);
             }
     }
+
+    public void showDireccionRetiro(View V){
+        if(Retiro_en_persona.isChecked()){
+            layoutRetiro.setVisibility(View.VISIBLE);
+        }
+        else{
+            layoutRetiro.setVisibility(View.GONE);
+        }
+    }
+
     public void updateButtonPublicarState(View V){
         if(Terminos_Condiciones.isChecked()){
             buttonPublicar.setEnabled(true);
@@ -63,6 +100,8 @@ public class MainActivity extends AppCompatActivity {
             buttonPublicar.setEnabled(false);
         }
     }
+
+
     public void Enviar(View V){
         if(Titulo.getText().toString().isEmpty()){
             Toast.makeText(this, "El campo Titulo es obligatorio", Toast.LENGTH_LONG).show();
@@ -104,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "El campo Dirección de retiro contiene caracteres no permitidos", Toast.LENGTH_LONG).show();
             return;
         }
-        if(!Descripcion.getText().toString().matches(validador)){
+        if(!Descripcion.getText().toString().matches(validador) && !Descripcion.getText().toString().isEmpty()){
             Toast.makeText(this, "El campo Descripción contiene caracteres no permitidos", Toast.LENGTH_LONG).show();
             return;
         }
